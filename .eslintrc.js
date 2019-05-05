0640053c7d7c298765825ee11a5ec4a604998c82
r-asayama <riku@b-assa.co.jp>
2019-05-05 20:32:21 +0900

    add: npm install

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,10 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    'standard',
+    'plugin:prettier/recommended'
   ],
   // required to lint *.vue files
   plugins: [
@@ -24,6 +25,14 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ],
   }
 }
